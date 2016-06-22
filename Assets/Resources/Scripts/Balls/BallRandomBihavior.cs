98fcfd4ad55f95ee1549b4116d9e08e271ecdbfa
VasiliyShevcheinko <vasiliy_shev@rambler.ru>
2016-06-22 15:41:09 +0545

    Update BallRandomBihavior.cs

--- Assets/Resources/Scripts/Balls/BallRandomBihavior.cs
+++ Assets/Resources/Scripts/Balls/BallRandomBihavior.cs
@@ -11,32 +11,38 @@ public class BallRandomBihavior : BallBihavior {
 	#region implemented abstract members of BallBihavior
 	protected override void BallUpdate ()
 	{
-		if(state==0)//выбираем очередную позицию
+		switch(state)
 		{
-			currPos=transform.position;
-			currX=currPos.x;
-			currY=currPos.y;
-
-			nextX=Random.Range(-2.3f,2.3f);
-			nextY=currY-Random.Range(0.5f,1.5f);
-			nextPos=new Vector3(nextX,nextY,0);
-			pause=Random.Range(0.5f,1.5f);
-			k=0;
-			state=1;
-		}
-		if(state==1)//перемещаемся в новую точку
-		{
-			transform.position=Vector3.Slerp(currPos,nextPos,k);
-			k+=dk;
-			if(k>1)
-				state=2;
-		}
-		if(state==2)//пауза между перемещениями
-		{
-			if(pause<0)
-				state=0;
-			else
-				pause-=Time.deltaTime;
+			case 0://выбираем очередную позицию
+			{
+				currPos=transform.position;
+				currX=currPos.x;
+				currY=currPos.y;
+	
+				nextX=Random.Range(-2.3f,2.3f);
+				nextY=currY-Random.Range(0.5f,1.5f);
+				nextPos=new Vector3(nextX,nextY,0);
+				pause=Random.Range(0.5f,1.5f);
+				k=0;
+				state=1;
+				break;
+			}
+			case 1://перемещаемся в новую точку
+			{
+				transform.position=Vector3.Slerp(currPos,nextPos,k);
+				k+=dk;
+				if(k>1)
+					state=2;
+				break;
+			}
+			case 2://пауза между перемещениями
+			{
+				if(pause<0)
+					state=0;
+				else
+					pause-=Time.deltaTime;
+				break;
+			}
 		}
 	}
 	#endregion
