186ad2c697f6493ea4d1c7747dff9a26cae3e9f9
VasiliyShevcheinko <vasiliy_shev@rambler.ru>
2016-07-01 17:54:27 +0545

    Update MenuBihavior.cs

--- Assets/Resources/Scripts/MenuBihavior.cs
+++ Assets/Resources/Scripts/MenuBihavior.cs
@@ -3,16 +3,6 @@ using System.Collections;
 
 public class MenuBihavior : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-	}
-
 	public void OnPlay()
 	{
 		Application.LoadLevel("game");
